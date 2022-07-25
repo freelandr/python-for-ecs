f541e67ae6f7522540d2e4d4fdd2b926e2c068f4
CLIDev2 <CLIDev2@wow.com>
2022-07-25 17:38:41 +0000

    updates

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hellow, ECS!'
+customers = [
+    {'id': 1, 'firstname': 'Paul', 'surname': 'Heath'},
+    {'id': 2, 'firstname': 'Colin', 'surname': 'Trap'},
+    {'id': 3, 'firstname': 'Lisa', 'surname': 'Noble'}
+]
+
+@app.route('/health')
+def health():
+    return 'Healthcheck status ok'
+
+@app.route('/customers')
+def get_customers():
+    return jsonify(customers)
+
+@app.route('/customers', methods=['POST'])    
+def add_customer():
+    customers.append(request.get_json())
+    return '', 204
+
+@app.route('/customers/<int:id>', methods=['DELETE'])
+def delete_customer(id:int):
+    global customers
+    cust_to_delete = next(filter(lambda cust: cust['id'] == id, customers), None)
+    if cust_to_delete:
+        print(f'deleting customer {cust_to_delete}')
+        customers.remove(cust_to_delete)
+        return f'deleted customer with id {id}', 204
+    else:
+         return 'customer not found', 404
+    
